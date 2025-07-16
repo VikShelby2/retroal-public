45b3a8be7edb05445ce8d7e7e5e16f60f60bb848
VikShelby2 <flyshopinc@gmail.com>
2025-07-16 23:54:00 +0200

    done k

--- app/components/HeroCarousel.tsx
+++ app/components/HeroCarousel.tsx
@@ -10,7 +10,7 @@ const heroData = {
   title: "Timeless Vintage",
   subtitle: "Curated pieces from decades past",
   cta: "Shop Now",
-  link: '/shop-all'
+  link: '/collections'
 }
 
 export default function HeroSection() {
